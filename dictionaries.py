ef1f6045a3ee8e3172eb58be9a86c4449a8812c8
KRP96 <kavindapeiris1996@gmail.com>
2021-07-08 13:36:59 +0530

    added new line

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,6 @@
 houses = {"Harry":"Gryffindor", "Draco":"Slytherin"}
 
+houses ["Hermione"] = "Gryffindor"         #Hermione is the dictionary and Gryffindor is the value
+
 print(houses["Harry"])        #print key value
+print(houses["Hermione"])
