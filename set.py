17d3ecf7bc64e14b31ce3fa7bc1f1c42316df870
KRP96 <kavindapeiris1996@gmail.com>
2021-07-08 13:07:26 +0530

    elements count

--- set.py
+++ set.py
@@ -9,3 +9,9 @@ s.add(4)
 s.add(4)       #set isn't allowed any duplicate values
 
 print(s)
+
+s.remove(3)      #remove number 3
+
+print(s)
+
+print(f"This set has {len(s)} elements.")    #count of elements
