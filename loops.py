6f1c153f831027b6b61e956a72d7a40625cf09b7
KRP96 <kavindapeiris1996@gmail.com>
2021-07-08 13:24:46 +0530

    add new lines

--- loops.py
+++ loops.py
@@ -3,5 +3,12 @@ for i in [0, 1, 2, 3, 4, 5]:
 
 print("=================================================")
 
-for i in range(100):
+for i in range(5):
     print(i)
+
+print("==========================================")
+
+names = ["Harry", "Ron", "Hermoine"]
+
+for name in names:
+    print(name)                 #print line by line
