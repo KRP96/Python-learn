b3cb622ea9d24145fe27534cbe916528358d54bc
KRP96 <kavindapeiris1996@gmail.com>
2021-07-08 12:55:52 +0530

    modify lists

--- list.py
+++ list.py
@@ -1,5 +1,9 @@
 # Define a list of names
 names= ["Harry", "Ron", "Hermione", "Ginny"]
 
-print(names)
-print(names[2])
+print(names)        #print list of names
+print(names[2])      #print third item
+
+names.append("Draco")       #add new list item
+names.sort()      #modify the order
+print(names)      #print modified list of names
