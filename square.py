0f84b5e02c4eb4fac9264c381ebb2de00f83579a
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 09:54:10 +0530

    add second methos for import functions

--- square.py
+++ square.py
@@ -1,3 +1,12 @@
 from functions import square
 for i in range(10):
     print(f"This square of  {i} is {square(i)}")
+
+
+print("                                                                      ")
+print("======================================================================")
+#second method
+import functions
+
+for i in range(10):
+    print(f"The square of {i} is {functions.square(i)}")
