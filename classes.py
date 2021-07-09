71ece93cf2d93350c7ebf6d89bf5eaa38b1a7048
KRP96 <kavindapeiris1996@gmail.com>
2021-07-09 10:09:10 +0530

    add classes.py file

---
+++ classes.py
@@ -0,0 +1,9 @@
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1               #store the value of input1 in the "self" object
+        self.y = input2               #store the value of input2 in the "self" object
+
+
+p = Point(2, 8)           #asign the point values into input1 and input2
+print(p.x)               #then print value of x in p variable
+print(p.y)               # print value of y in p variable
